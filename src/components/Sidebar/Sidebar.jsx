7194d4e863f11437bb44bfb6a1f156f5e48ec657
anastasiia.tarasiuk <anastasiia.tarasiuk@iqvia.com>
2023-05-10 15:16:21 +0300

    Bugfix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -40,8 +40,8 @@ export const Sidebar = ({onClick}) => {
 
     return <StyledSidebar className="moveSidebarBack" ref={refSideBar} onMouseOver={()=>setOver(true)}
                           onMouseOut={()=>setOver(false)}>
-        {window.innerWidth < 1024 && <MobileButton onClick={onMobileButtonClick} isSideBarOpen={isSideBarOpen}/>}
-        {window.innerWidth >= 1024 && !over && <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></Icon>}
+        {window.innerWidth < 1024 && <MobileButton aria-label="Button to interact with sidebar" onClick={onMobileButtonClick} isSideBarOpen={isSideBarOpen}/>}
+        {window.innerWidth >= 1024 && !over && <Icon aria-label="Icon to interact with sidebar" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></Icon>}
         <Message text={'Manage your playlist'}/>
         <form>
             <FormInput labelText={"Type to find your video"} onChange={e=> filterVideos(e)}/>
